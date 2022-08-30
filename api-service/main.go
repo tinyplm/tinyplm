4727ce259e1dc127d4f21326048312a949102bba
Sandeep Kongathi <kn.sandeep@gmail.com>
2022-08-30 10:26:01 +0530

    updated

--- api-service/main.go
+++ api-service/main.go
@@ -1,13 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+
+	r.GET("/colors", getColors)
 
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "tinyplm!")
 	})
 
 	r.Run(":9000")
+}
+
+func getColors(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, colors)
+}
+
+type color struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	RGB         string `json:"rgb"`
+	Description string `json:"description"`
+}
+
+var colors = []color{
+	{ID: "1", Name: "Blue Train", RGB: "John Coltrane", Description: "56.99"},
+	{ID: "2", Name: "Jeru", RGB: "Gerry Mulligan", Description: "17.99"},
+	{ID: "3", Name: "Sarah Vaughan and Clifford Brown", RGB: "Sarah Vaughan", Description: "39.99"},
 }
